259fd2e6f885433c55e1a531e7c72b51feb3eb4c
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-18 20:36:57 +0200

    Update Main.java

--- Triangle/Main.java
+++ Triangle/Main.java
@@ -4,17 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Triangle triangle = new Triangle(5f,8f,6f, 5.99f);
-        TriangleEqualSides triangleEqualSides = new TriangleEqualSides(5f,8f,3f);
+        Triangle triangle = new Triangle(5f,8f,6f);
+        TriangleEqualSides triangleEqualSides = new TriangleEqualSides(5f,8f);
         TriangleWith90 triangleWith90 = new TriangleWith90(2f,3f,4f);
+        TriangleAllEqualSides triangleAllEqualSides = new TriangleAllEqualSides(5.5f);
 
         System.out.println(String.format("Perimeter of triangle %.2f", triangle.getHekef()));
         System.out.println(String.format("Area of triangle %.2f", triangle.getArea()));
 
         System.out.println(String.format("Perimeter of triangle Equal Sides %.2f", triangleEqualSides.getHekef()));
-        System.out.println(String.format("Area of triangle Equal Sides %f", triangleEqualSides.getArea()));
+        System.out.println(String.format("Area of triangle Equal Sides %.2f", triangleEqualSides.getArea()));
 
         System.out.println(String.format("Perimeter of triangle With 90 %.2f", triangleWith90.getHekef()));
         System.out.println(String.format("Area of triangle With 90 %.2f", triangleWith90.getArea()));
+
+        System.out.println(String.format("Perimeter of triangle All Equal Sides %.2f", triangleAllEqualSides.getHekef()));
+        System.out.println(String.format("Area of triangle All Equal Sides %.2f", triangleAllEqualSides.getArea()));
     }
 }
+
