5832df9dcc9496f3dc987ba5129aeabc462e622d
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-18 20:38:17 +0200

    Update TriangleEqualSides.java

--- Triangle/TriangleEqualSides.java
+++ Triangle/TriangleEqualSides.java
@@ -2,8 +2,8 @@ package com.app;
 
 public class  TriangleEqualSides extends Triangle {
 
-    public TriangleEqualSides(float m_a, float m_c, float m_h) {
-        super(m_a, m_a, m_c, m_h);
+    public TriangleEqualSides(float m_a, float m_c) {
+        super(m_a, m_a, m_c);
     }
 
     @Override
